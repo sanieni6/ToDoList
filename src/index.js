ecbf723189eefc31ccfbc44a9fbd6b8e4872c9f2
sanieni6 <sanieni6@gmail.com>
2023-02-22 14:21:22 -0500

    Add main source files of the project

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line no-unused-vars
+import _ from 'lodash';
+import './style.css';
+
+const list = document.querySelector('.list');
+
+const tasks = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Read ten pages',
+    completed: false,
+    index: 1,
+
+  },
+  {
+    description: 'do homework',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'finish the capstone project',
+    completed: false,
+    index: 3,
+  },
+];
+
+tasks.forEach((element) => {
+  list.insertAdjacentHTML('beforeend', `
+    <li class="element">
+                <input class="status" type="checkbox">
+                <textarea name="task" id="task" maxlength="200">${element.description}</textarea>
+                <span class="material-icons">more_vert</span>
+            </li>
+    `);
+});
