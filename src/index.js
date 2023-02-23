10bfdc5ea01f3b20ca847e7331bc40b1a67565d6
sanieni6 <sanieni6@gmail.com>
2023-02-23 11:52:09 -0500

    Add new project requeriments

--- src/index.js
+++ src/index.js
@@ -3,39 +3,24 @@
 // eslint-disable-next-line import/no-unresolved
 import _ from 'lodash';
 import './style.css';
+import * as crud from './modules/crud.js';
 
-const list = document.querySelector('.list');
+const textInput = document.querySelector('.text-input');
 
-const tasks = [
-  {
-    description: 'wash the dishes',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Read ten pages',
-    completed: false,
-    index: 1,
+const form = document.querySelector('.forma');
+const index = 0;
 
-  },
-  {
-    description: 'do homework',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'finish the capstone project',
-    completed: false,
-    index: 3,
-  },
-];
+window.onload = crud.create();
 
-tasks.forEach((element) => {
-  list.insertAdjacentHTML('beforeend', `
-    <li class="element">
-                <input class="status" type="checkbox">
-                <textarea name="task" id="task" maxlength="200">${element.description}</textarea>
-                <span class="material-icons">more_vert</span>
-            </li>
-    `);
+form.addEventListener('submit', (event) => {
+  const task = {};
+  task.description = textInput.value;
+  task.completed = false;
+  task.index = index;
+  crud.tasks.push(task);
+  crud.updateIndexes();
+  crud.saveLocalStorage();
+  crud.create();
+  textInput.value = '';
+  event.preventDefault();
 });
