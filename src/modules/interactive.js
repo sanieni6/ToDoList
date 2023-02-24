54ab68d16633f24c74d35944855ce0d9816abc3c
sanieni6 <sanieni6@gmail.com>
2023-02-24 08:56:56 -0500

    Solve linters

--- src/modules/interactive.js
+++ src/modules/interactive.js
@@ -3,14 +3,13 @@ export const ckeckboxState = (tasks, remove, element, index) => {
     tasks[index].completed = true;
     remove[index].style.textDecoration = 'line-through';
     return tasks;
-  } 
-    tasks[index].completed = false;
-    remove[index].style.textDecoration = 'none';
-    return tasks;
-  
+  }
+  tasks[index].completed = false;
+  remove[index].style.textDecoration = 'none';
+  return tasks;
 };
 
 export const deleteChecked = (tasks) => {
   tasks = tasks.filter((task) => !task.completed);
   return tasks;
-}
+};
